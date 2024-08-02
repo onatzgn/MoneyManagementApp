4728fa6ff0eceeb583e2b18964fab9f9d1af2ae3
Fatma Yılmaz <79848362+fatmayilmaz4101@users.noreply.github.com>
2024-08-02 15:36:01 +0300

    update Read.ME

--- src/assets/Images/index.ts
+++ src/assets/Images/index.ts
@@ -3,6 +3,7 @@ const Images = {
   starIcon: require('./starIcon.png'),
   timoLogo: require('./timoLogo.png'),
   timoChatIcon: require('./timoChatIcon.png'),
+  avatar: require('./avatar.png')
 };
 
 export default Images;
